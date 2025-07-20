9fb66d451d7735c751741c877168c1e4413f09f6
Jason <jasonretro580@gmail.com>
2025-07-20 20:58:49 +0300

    Added new to changes

---
+++ alx-react-app-new/alx-react-app/src/components/Footer.jsx
@@ -0,0 +1,10 @@
+function Footer() {
+    return(
+        <div>
+            <footer style={{ backgroundColor: 'navy', color: 'white', textAlign: 'center' }}>
+                <p><span style={{ fontWeight: 'bold' }}>© 2023 City Lovers</span></p>
+            </footer>
+        </div>
+    )
+}
+export default Footer;
---
+++ alx-react-app-new/alx-react-app/src/components/Header.jsx
@@ -0,0 +1,10 @@
+function Header() {
+    return(
+        <div style={{ backgroundColor: 'navy', color: 'white', textAlign: 'center' }}>
+            <header>
+                <h1>My Favorite Cities</h1>
+            </header>
+        </div>
+    );
+}
+export default Header;
