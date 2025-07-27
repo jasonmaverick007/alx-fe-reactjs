f2d95259e9e64025b2e4468bf874617bc323e7af
Jason <jasonretro580@gmail.com>
2025-07-27 19:32:05 +0300

    Added Counter.jsx

--- alx-react-app-new/src/App.jsx
+++ alx-react-app-new/src/App.jsx
@@ -3,10 +3,11 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import WelcomeMessage from './components/WelcomeMessage'
-import Header from './components/header'
+import Header from './components/Header'
 import MainContent from './components/MainContent'
 import Footer from './components/Footer'
 import UserProfile from './components/UserProfile'
+import Counter from './components/Counter'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -45,6 +46,7 @@ function App() {
       <MainContent />
       <Footer />
       <UserProfile />
+      <Counter/>
     </>
   )
 }
