fcbeeb96f7d3e4e2132c17bc88d66d0dfff360b3
Jason <jasonretro580@gmail.com>
2025-08-10 21:58:51 +0300

    re-added github-user-search

--- github-user-search/src/App.jsx
+++ github-user-search/src/App.jsx
@@ -1,13 +1,18 @@
-// src/App.jsx
 import React from "react";
+import SearchBar from "./components/SearchBar";
 
 function App() {
+  const handleSearch = (username) => {
+    console.log("Searching for:", username);
+    // API call will go here later
+  };
+
   return (
-    <div>
+    <div style={{ padding: "2rem" }}>
       <h1>GitHub User Search</h1>
+      <SearchBar onSearch={handleSearch} />
     </div>
   );
 }
 
 export default App;
-
