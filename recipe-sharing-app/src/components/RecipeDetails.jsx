fced84232cf50a6b4f0fd689b406c43c96588dc9
Jason <jasonretro580@gmail.com>
2025-08-10 18:32:16 +0300

    Added recipe-sharing-app

--- recipe-sharing-app/src/components/RecipeDetails.jsx
+++ recipe-sharing-app/src/components/RecipeDetails.jsx
@@ -2,9 +2,9 @@ import { useRecipeStore } from './recipeStore';
 import EditRecipeForm from './EditRecipeForm';
 import DeleteRecipeButton from './DeleteRecipeButton';
 
-const RecipeDetails = ({ recipeId }) => {
+const RecipeDetails = ({ recipeid }) => {
     const recipe = useRecipeStore(state =>
-        state.recipes.find(recipe => recipeId.id === recipeId)
+        state.recipes.find(recipe => recipeid.id === recipeid)
     );
 
 
@@ -13,7 +13,7 @@ const RecipeDetails = ({ recipeId }) => {
             <h1>{recipe.title}</h1>
             <p>{recipe.description}</p>
             <EditRecipeForm/>
-            <DeleteRecipeButton recipeId={recipe.Id} onDelete={() => NavigationHistoryEntry('/')}/>
+            <DeleteRecipeButton recipeid={recipe.Id} onDelete={() => NavigationHistoryEntry('/')}/>
         </div>
     );
 }
